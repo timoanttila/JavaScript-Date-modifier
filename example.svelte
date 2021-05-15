a887e545c9ce13a0ecdd81d9c0604d74d71ac81a
Timo Anttila <14306628+timoanttila@users.noreply.github.com>
2021-05-15 22:29:38 +0300

    Create example.svelte

---
+++ example.svelte
@@ -0,0 +1,13 @@
+<script>
+	import Datetime from './date.svelte';
+	let dates = [
+		"2021-05-09 10:37:41",
+		"2021-06-21 15:48:21"
+	];
+</script>
+
+{#each dates as item}
+	<div class="date">
+		<Datetime {item} i="1"/>
+	</div>
+{/each}
