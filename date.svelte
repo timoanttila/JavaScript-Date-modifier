8f434cee45f924f0294d50722841db6515fb2475
Timo Anttila <14306628+timoanttila@users.noreply.github.com>
2021-05-15 22:04:03 +0300

    Create date.svelte

---
+++ date.svelte
@@ -0,0 +1,17 @@
+<script>
+	export let item;
+	export let i = '';
+	export let lang = 'fi-FI';
+	let options = {
+		year: 'numeric',
+		month: '2-digit',
+		day: '2-digit',
+		hour: '2-digit',
+		minute: '2-digit',
+	};
+	if (i == 1) options.weekday = 'short';
+	let date = new Date(item).toLocaleDateString(lang, options);
+	date = !i ? date.split(' ')[0] : date.replaceAll(' klo ', ' ');
+</script>
+
+{date}
